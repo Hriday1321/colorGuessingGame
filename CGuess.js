ebd0f3bb0a676a51991db6188316a3f208f9b453
Hriday Gupta <hridaygupta100@hotmail.com>
2021-05-22 21:04:54 +0530

    First proper commit

---
+++ CGuess.js
@@ -0,0 +1,76 @@
+var diff = 6;
+var colors = generateRandomColors(diff);
+var squares = document.querySelectorAll('.square');
+var goal = colors[Math.floor(Math.random() * colors.length)];
+var chosen = document.querySelector('#chosen');
+var msg = document.querySelector('#msg');
+var h1 = document.querySelector('h1');
+var reset = document.querySelector('#reset');
+var hard = document.querySelector('#hard');
+var easy = document.querySelector('#easy');
+
+
+chosen.textContent = "RGB" + goal.slice(3);
+
+for(var i=0; i < squares.length; i++)
+{
+	squares[i].style.backgroundColor = colors[i];
+	squares[i].addEventListener("click", function(){
+		var clickedColor = this.style.backgroundColor;
+		if(clickedColor === goal)
+		{
+			msg.textContent = 'Success!';
+			correct(clickedColor);
+			h1.style.backgroundColor = clickedColor;
+		}
+		else
+		{
+			this.style.backgroundColor = '#555555'; 
+			msg.textContent = 'Try Again';
+		}
+	})
+}
+
+function correct(color){
+	for(var i = 0; i < colors.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+	reset.textContent = 'Try Again';
+}
+
+function generateRandomColors(num){
+	var arr = [];
+	for(var i = 0; i < num; i++){
+		arr[i] = "rgb("+ Math.floor(Math.random() * 256) + ", "+ Math.floor(Math.random() * 256) +", " + Math.floor(Math.random() * 256) + ")";
+	}
+	return arr;
+}
+
+reset.addEventListener('click', resetting);
+hard.addEventListener('click', function(){
+		hard.classList.add('selected');
+		easy.classList.remove('selected');	
+		diff = 6;
+		resetting();
+})
+easy.addEventListener('click', function(){
+		hard.classList.remove('selected');
+		easy.classList.add('selected');
+		diff = 3;
+		resetting();
+})
+
+function resetting(){
+	colors = generateRandomColors(diff);
+	goal = colors[Math.floor(Math.random() * colors.length)];
+	chosen.textContent = "RGB" + goal.slice(3);
+	for(var i = 0; i < squares.length; i++){
+		if(i < colors.length)
+			squares[i].style.backgroundColor = colors[i];
+		else
+			squares[i].style.backgroundColor = '#555555';
+	}
+	h1.style.backgroundColor = '#a83242';
+	reset.textContent = 'New Colors';
+	msg.textContent = '';
+}
